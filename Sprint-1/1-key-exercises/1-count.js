5f0acb1c7a03ea076f3330cd68f6bf72bcab3a82
Elena Vasileia <el1vas@yahoo.gr>
2025-10-16 22:32:36 +0200

    Update comments for count variable explanation
    Clarify comments explaining variable declaration and reassignment.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+//Line 2 is reassigning the value of the variable count adding 1 to the original value. The = operator is used for the reassignement of value
