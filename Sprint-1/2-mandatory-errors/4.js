732167cd842044c0eaa264f23dcc3bb81054de8c
Elena Vasileia <el1vas@yahoo.gr>
2025-10-17 14:29:22 +0200

    Fixed syntax error in the names of the variables
    Renamed variables to start with a letter not a number

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,13 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+console.log("12-hour clock time:", 12HourClockTime);
+//We get the below error:
+const 12HourClockTime = "20:53";
+      ^^
+
+SyntaxError: Invalid or unexpected token
+//Because we can't start a name of a variable with a number (it applies in both variables above)
+//So a suggestion could be:
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
