c45da88e63f9a15362d2044ff9cb3028942f3c31
Elena Vasileia <el1vas@yahoo.gr>
2025-10-17 14:54:24 +0200

    Answered to all questions
    Found the function calls, identified the error and fixed it, found where variables are reassigned and where they were declared and explained why we have to remove the comma from a string "10,000" before we turn it into number.

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,16 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
+//Two funcion calls in lines 4 and 5 (Number and .replaceAll) and one funcion call in line 10 (.log)
 
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+//I got a syntax error because in line 5 I was missing a comma between "," and "". I added it.
 
 // c) Identify all the lines that are variable reassignment statements
+//Lines 4 and 5
 
 // d) Identify all the lines that are variable declarations
+//Lines 1,2 and 7 and 8
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+//It removes the comma from the string 10,000 so it can turn it to a number.
