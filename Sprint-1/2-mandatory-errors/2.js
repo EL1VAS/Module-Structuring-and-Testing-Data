4ce31e5190968302ccb86371acb0dc7287e25be2
Elena Vasileia <el1vas@yahoo.gr>
2025-10-17 13:44:12 +0200

    Moved the variable before the console.log
    Moved the declaration of cityOfBirth before its usage in console.log.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+//JavaScript program reads code line by line so below we loged in the console something that wasn't defined yet, the cityOfBirth variable. 
+//I cut and pasted the line of the variable before the console.log
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
