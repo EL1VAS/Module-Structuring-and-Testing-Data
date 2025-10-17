ff439d6975a0810bba4742d0d3173106c266ba56
Elena Vasileia <el1vas@yahoo.gr>
2025-10-17 13:41:20 +0200

    Change age variable from const to let
    Changed 'const' to 'let' for the age variable to allow reassignment.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,4 @@
 // trying to create an age variable and then reassign the value by 1
-
-const age = 33;
+//For variables that can be reassigned in the future we use let instead of const
+let age = 33;
 age = age + 1;
