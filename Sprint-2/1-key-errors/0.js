c611558b88287d6dd1304895025ea557940deefe
Elena Vasileia <el1vas@yahoo.gr>
2025-10-20 21:51:37 +0200

    Fix redeclaration error in capitalise function
    Removed redeclaration of 'str' variable in the 'capitalise' function.

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I think that this  is a function in order to capitalize the first letter of any string defined by the variable str
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -9,5 +9,20 @@ function capitalise(str) {
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> I get the following error message:
+// SyntaxError: Identifier 'str' has already been declared
+//    at wrapSafe (node:internal/modules/cjs/loader:1691:18)
+//    at Module._compile (node:internal/modules/cjs/loader:1734:20)
+//    at Object..js (node:internal/modules/cjs/loader:1893:10)
+//    at Module.load (node:internal/modules/cjs/loader:1480:32)
+//    at Module._load (node:internal/modules/cjs/loader:1299:12)
+//    at TracingChannel.traceSync (node:diagnostics_channel:328:14)
+//    at wrapModuleLoad (node:internal/modules/cjs/loader:244:24)
+ //   at Module.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:154:5)
+ //   at node:internal/main/run_main_module:33:47
+//I realize that by adding a parameter in the function capitalize IS actually declaring the variable str, so we can't redeclate it below with let. I will remove the let and check again.
+
+function capitalise(str) {
+   str = `${str[0].toUpperCase()}${str.slice(1)}`;
+   return str;
+}
