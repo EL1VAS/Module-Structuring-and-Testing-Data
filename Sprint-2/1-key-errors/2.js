4abba41eb44f108f7e21192f315e76512f6ba8bb
Elena Vasileia <el1vas@yahoo.gr>
2025-10-20 22:40:50 +0200

    Fix square function to use parameter 'num'
    Corrected the square function to accept a parameter and return its square.
    Tested it by loging the functions value in the console

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,30 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
-
+// =============> The num is not defined.
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
+// =============>SyntaxError: Unexpected number
+//    at wrapSafe (node:internal/modules/cjs/loader:1691:18)
+//    at Module._compile (node:internal/modules/cjs/loader:1734:20)
+//    at Object..js (node:internal/modules/cjs/loader:1893:10)
+//    at Module.load (node:internal/modules/cjs/loader:1480:32)
+//    at Module._load (node:internal/modules/cjs/loader:1299:12)
+ //   at TracingChannel.traceSync (node:diagnostics_channel:328:14)
+ //   at wrapModuleLoad (node:internal/modules/cjs/loader:244:24)
+ //   at Module.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:154:5)
+ //   at node:internal/main/run_main_module:33:47
 
-// =============> explain this error message here
+//Node.js v24.10.0
+
+// =============> Number 3 was unexpected number where it supposed to be a parameter num
 
 // Finally, correct the code to fix the problem
-
-// =============> write your new code here
+function square(num) {
+    return num * num;
+}
+console.log(square(5)); //It returned 25 so it works
 
 
