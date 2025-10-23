f0e3ff6873b54b58a36a6b6cc69ab9489ad0b589
Elena Vasileia <el1vas@yahoo.gr>
2025-10-23 15:14:03 +0200

    Fix multiply function to return product
    Updated the multiply function to return the product instead of logging it, fixing the undefined result issue.

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============>I think that instead of console.log(a*b) we should ask for return(a*b). I think we will get an undifined error for a and b (but I am not completely sure because a and b are parameters.)
 
 function multiply(a, b) {
   console.log(a * b);
@@ -8,7 +8,14 @@ function multiply(a, b) {
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// =============> Well my prediction was right partially. There should have been a return (a*b) in the function instead of console.log. 
+//What I couldn't understand is that I did receive a result of 320 and then "The result of multiplying 10 and 32 is undefined".
+//So, it took the numbers 10 and 32 to define the parameters from out of the function but then because there is no return it showed undefined? I would like to analyze this in class.
+//I will try more to play with the code.
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+function multiply(a, b) {
+  return(a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
