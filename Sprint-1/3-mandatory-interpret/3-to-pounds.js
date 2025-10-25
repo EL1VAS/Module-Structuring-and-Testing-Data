3a4cb84792d7fce8ff332ae3a8af390e6bc65b82
Elena Vasileia <el1vas@yahoo.gr>
2025-10-25 10:00:48 +0200

    Explained the code from pence to pound.
    Added comments to clarify the purpose of each variable and operation in the conversion from pence to pounds.

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -1,19 +1,21 @@
-const penceString = "399p";
+const penceString = "399p"; // Initialises a string variable with the value "399p"
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
   penceString.length - 1
-);
+); //Removes the p from the string by choosing only the string that is between location 0 & -1. p is at location  after -1.
 
-const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");//This variable makes sure we have at least 3 digits in our string, by adding two 0s in front.
+//However I don't understand why is needed since we have already defined our penceString variable with const, therefore the value is 399p, which makes penceStringWithoutTrailingP already a 3 digit string.
 const pounds = paddedPenceNumberString.substring(
   0,
   paddedPenceNumberString.length - 2
-);
+); // This variable is extracting the pound part of our string by taking a substring from it's start location 0 till location -2 (where the last two digits of pence are).
 
 const pence = paddedPenceNumberString
   .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+  .padEnd(2, "0"); // This variable takes the last two digits of the string and with padEnd makes sure there are always two digits there. 
+//However again I don't understand why is this needed in our exaple here since all variables are const and not let, therefore with stable value and 399 has already 2 digits at the end.
 
 console.log(`£${pounds}.${pence}`);
 
@@ -23,5 +25,4 @@ console.log(`£${pounds}.${pence}`);
 // You need to do a step-by-step breakdown of each line in this program
 // Try and describe the purpose / rationale behind each step
 
-// To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+
