b21cdd4689322be03c241c06a22072c935642a5a
Pythia <el1vas@yahoo.gr>
2025-10-31 17:35:09 +0100

    Copied function from my prep case, added tests and fixed typos after running the tests

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,20 @@
-function getOrdinalNumber(num) {
-  return "1st";
+function getOrdinalNumber(n) {
+  const rem100 = n % 100;
+  if (rem100 >= 11 && rem100 <= 13) {
+    return n + "th";
+  }
+
+  const rem10 = n % 10;
+  if (rem10 === 1) {
+    return n + "st";
+  }
+  if (rem10 === 2) {
+    return n + "nd";
+  }
+  if (rem10 === 3) {
+    return n + "rd";
+  }
+  return n + "th";
 }
 
 module.exports = getOrdinalNumber;
