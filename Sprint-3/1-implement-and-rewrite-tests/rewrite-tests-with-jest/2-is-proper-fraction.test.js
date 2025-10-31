21bcb1a168e5a864ff58653d1fd791bd3adde8f6
Pythia <el1vas@yahoo.gr>
2025-10-31 16:28:58 +0100

    Added tests for all cases of proper/improper fractions

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -7,7 +7,21 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for a negative proper fraction", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false when numerator equals denominator", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
+
+// Stretch Case: Negative Denominator check
+test("should return true for a proper fraction with a negative denominator", () => {
+  expect(isProperFraction(3, -7)).toEqual(true);
+});
