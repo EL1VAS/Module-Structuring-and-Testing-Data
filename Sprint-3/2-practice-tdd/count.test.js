0c45a31063e6a7b14de5b63c5f317c03112ba201
Pythia <el1vas@yahoo.gr>
2025-10-31 17:19:08 +0100

    Wrote tests on some scenarios and completed the function to pass the tests

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,33 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should return 0 when character does not exist in string", () => {
+  const str = "AAAAA";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+// Scenario: Single Occurrence
+// Given the input string str,
+// And a character char that occurs exactly once within str,
+// When the function is called with these inputs,
+// Then it should return 1, indicating a single occurrence of char in str.
+test("should return 1 when character occurs once in string", () => {
+  const str = "Hello";
+  const char = "H";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
+
+// Scenario: Empty String
+// Given an empty input string str,
+// And any character char,
+// When the function is called with these inputs,
+// Then it should return 0, indicating that no occurrences of char were found in the empty str.
+test("should return 0 when input string is empty", () => {
+  const str = "";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
