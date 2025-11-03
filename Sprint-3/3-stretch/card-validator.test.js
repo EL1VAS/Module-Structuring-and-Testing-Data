c7cd7bcb64484f80fbef9f8c776eb6765fbb8930
Pythia <el1vas@yahoo.gr>
2025-11-03 20:21:28 +0100

    Created one test

--- Sprint-3/3-stretch/card-validator.test.js
+++ Sprint-3/3-stretch/card-validator.test.js
@@ -1,2 +1,4 @@
 const cardValidator = require("./card-validator");
-test;
+test (('valid 16digit card number passes validation'), () => {
+    expect(cardValidator(2549876514523548)).toEqual("Valid card number")
+});
